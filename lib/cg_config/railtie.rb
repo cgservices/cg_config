0a59673ded44c3fc0ce655f320afc5d2850e050a
Luis Doubrava <luis@cg.nl>
2013-01-04 13:22:13 +0100

    recursively symbolize keys

--- lib/cg_config/railtie.rb
+++ lib/cg_config/railtie.rb
@@ -1,10 +1,25 @@
 # Loop through CgConfig::CG_CONFIG_FOLDER to to load all yml files into constants
 module CgConfig
   class CgConfigRailtie < Rails::Railtie
+    def recursive_symbolize_keys(h)
+      case h
+        when Hash
+          Hash[
+              h.map do |k, v|
+                [ k.respond_to?(:to_sym) ? k.to_sym : k, recursive_symbolize_keys(v) ]
+              end
+          ]
+        when Enumerable
+          h.map { |v| recursive_symbolize_keys(v) }
+        else
+          h
+      end
+    end
+
     initializer "cg_config_railtie.configure_rails_initialization" do |app|
       CgConfig.const_set('CG_CONFIG_FOLDER', '/config/yml') unless CgConfig.constants.include?('CG_CONFIG_FOLDER')
 
-      puts "Loading yml config files from #{app.root}/#{CgConfig::CG_CONFIG_FOLDER}..."
+      puts "Loading yml config files from #{app.root}#{CgConfig::CG_CONFIG_FOLDER}..."
       Dir.glob("#{app.root}/#{CgConfig::CG_CONFIG_FOLDER}/*.yml") do |yml_file|
 
         yml_config = YAML.load_file(yml_file)
@@ -13,7 +28,7 @@ module CgConfig
 
         puts "Setting #{const_name}"
 
-        CgConfig.const_set(const_name, env_config.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo})
+        CgConfig.const_set(const_name, self.recursive_symbolize_keys(env_config))
       end
     end
   end
