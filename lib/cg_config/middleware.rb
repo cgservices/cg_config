bb4b18097aab35b8488785cb87a312aab5a39831
Luis Doubrava <luis@cg.nl>
2014-10-28 12:47:47 +0100

    Include config files from Application

---
+++ lib/cg_config/middleware.rb
@@ -0,0 +1,38 @@
+module CgConfig
+  class Middleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      CgConfig.const_set('CG_CONFIG_FOLDER', '/config/yml') unless CgConfig.constants.include?('CG_CONFIG_FOLDER')
+      Rails.application.railties.each do |railtie|
+        if railtie.respond_to?(:paths) && (path = railtie.root.to_s + CgConfig::CG_CONFIG_FOLDER)
+          Dir.glob(path + '/*.yml') do |yml_file|
+            yml_config = YAML.load_file(yml_file)
+            env_config = yml_config.has_key?(Rails.env) ? yml_config[Rails.env] : yml_config
+            const_name = File.basename(yml_file, ".yml").upcase
+            CgConfig.const_set(const_name, self.recursive_symbolize_keys(env_config))
+          end
+        end
+      end
+      @app.call(env)
+    end
+
+    private
+    def recursive_symbolize_keys(h)
+      case h
+        when Hash
+          Hash[
+              h.map do |k, v|
+                [ k.respond_to?(:to_sym) ? k.to_sym : k, recursive_symbolize_keys(v) ]
+              end
+          ]
+        when Enumerable
+          h.map { |v| recursive_symbolize_keys(v) }
+        else
+          h
+      end
+    end
+  end
+end
--- lib/cg_config/railtie.rb
+++ lib/cg_config/railtie.rb
@@ -16,20 +16,28 @@ module CgConfig
       end
     end
 
-    initializer "cg_config_railtie.configure_rails_initialization" do |app|
-      CgConfig.const_set('CG_CONFIG_FOLDER', '/config/yml') unless CgConfig.constants.include?('CG_CONFIG_FOLDER')
-      Rails.application.railties.each do |railtie|
-        if railtie.respond_to?(:paths) && (path = railtie.root.to_s + CgConfig::CG_CONFIG_FOLDER)
-          Dir.glob(path + '/*.yml') do |yml_file|
-            yml_config = YAML.load_file(yml_file)
-            env_config = yml_config.has_key?(Rails.env) ? yml_config[Rails.env] : yml_config
-            const_name = File.basename(yml_file, ".yml").upcase
-            CgConfig.const_set(const_name, self.recursive_symbolize_keys(env_config))
-          end
-        end
+    def load_yml_from_path(path)
+      Dir.glob(path + '/*.yml') do |yml_file|
+        yml_config = YAML.load_file(yml_file)
+        env_config = yml_config.has_key?(Rails.env) ? yml_config[Rails.env] : yml_config
+        const_name = File.basename(yml_file, ".yml").upcase
+        CgConfig.const_set(const_name, self.recursive_symbolize_keys(env_config))
       end
     end
 
+    initializer "cg_config_railtie.configure_rails_initialization" do |app|
+      CgConfig.const_set('CG_CONFIG_FOLDER', '/config/yml') unless CgConfig.constants.include?('CG_CONFIG_FOLDER')
+      # Load yml files from Application
+      path = Rails.application.root.to_s + CgConfig::CG_CONFIG_FOLDER
+      load_yml_from_path(path)
+
+      # Load yml files from railties
+      Rails.application.railties.each do |railtie|
+        if railtie.respond_to?(:paths) && (path = railtie.root.to_s + CgConfig::CG_CONFIG_FOLDER)
+          load_yml_from_path(path)
+        end
+      end
+    end
   end
 end
 
